02e5dc015cf17328b530ae10c85427e1567a8900
Evan Sosenko <razorx@evansosenko.com>
2014-08-03 16:16:52 -0700

    Set ext_conf_dir for Ubuntu 14.04.

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,6 +3,13 @@
 # Recipe:: default
 #
 
+# This is required to for PHP 5.4 on Ubuntu.
+# @todo Remove this when [php issue 76] is closed.
+# [php issue 76]: https://github.com/opscode-cookbooks/php/issues/76
+if platform?('ubuntu') && node['platform_version'].to_f >= 14.04
+  node.default['php']['ext_conf_dir'] = '/etc/php5/mods-available'
+end
+
 include_recipe 'php::default'
 
 node['php-modules']['modules'].each do |k, v|
